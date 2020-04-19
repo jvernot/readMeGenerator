3bc2e52ae9370186f3cfd3eefb9c912c76dbfc3b
Jason Vernot <jason.vernot@gmail.com>
2020-04-19 13:19:44 -0400

    added user questions

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-const questions = [
+// const Choices = require("inqirer/lib/objects/choices");
+var inquirer = require("inquirer");
 
+const questions = [
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub username?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "url",
+        message: "What is the URL to your project?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project called?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please write a description of your project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "Apache 2.0", "GPL 2.0", "BSD 3", "None"]
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "What command should be run to install dependencies?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "What command should be run to run tests?"
+    },
+    {
+        type: "input",
+        name: "running",
+        message: "What does the user need to know about using the repo?"
+    },
+    {
+        type: "input",
+        name: "contributions",
+        message: "What does the user need to know about contributing to the repo?"
+    },
 ];
+
+inquirer
+    .prompt(questions);
 
 function writeToFile(fileName, data) {
 }
