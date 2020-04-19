8517bf2b5ae0bfa84b98db0e5c847576698696fb
Jason Vernot <jason.vernot@gmail.com>
2020-04-19 12:17:57 -0400

    fixed files and added additional files

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const inquirer = require("inquirer");
-const fs = require("fs");
-const util = require("util");
+const questions = [
+
+];
+
+function writeToFile(fileName, data) {
+}
+
+function init() {
+
+}
+
+init();
